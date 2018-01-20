f77edefa018d09c27ce746788413a538354ed8f3
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-20 12:32:13 +0000

    Initial commit

---
+++ Smack-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack-App
+//
+//  Created by Hayden Jamieson on 20/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
