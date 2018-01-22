4ca65f0180fa27cf386893815c388625a7c78e60
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-22 18:11:04 +0000

    Configured parsing to retrieve channels
    Note, throwing error as array of channels initially empty - once adding channels is configured the error should dissappear.

---
+++ Smack-App/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  Smack-App
+//
+//  Created by Hayden Jamieson on 22/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+
+}
