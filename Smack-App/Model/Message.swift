61cb805d4e0b97acfacfbdda31fe4ad4ff71b2f6
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-23 15:49:39 +0000

    Configured JSON parsing to retrieve messages for channel
    Note, yet to implement adding messages to channel hence messages array empty

---
+++ Smack-App/Model/Message.swift
@@ -0,0 +1,22 @@
+//
+//  Message.swift
+//  Smack-App
+//
+//  Created by Hayden Jamieson on 23/01/2018.
+//  Copyright © 2018 Hayden Jamieson. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+   
+    
+}
