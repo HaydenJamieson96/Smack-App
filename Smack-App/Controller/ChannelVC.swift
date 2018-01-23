e6b088b3663a02e357840e574f86610c82fb91e9
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-01-23 15:26:29 +0000

    Configured Login Flow
    Added UI changes dependency changes, created 2 more notifications for observing selected channels and loading channels

--- Smack-App/Controller/ChannelVC.swift
+++ Smack-App/Controller/ChannelVC.swift
@@ -20,6 +20,7 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.channelsLoaded(_:)), name: NOTIF_CHANNELS_LOADED, object: nil)
         tableView.delegate = self
         tableView.dataSource = self
         SocketService.instance.getChannel { (success) in
@@ -34,9 +35,11 @@ class ChannelVC: UIViewController {
     }
 
     @IBAction func addChannelPressed(_ sender: Any) {
-        let addChannel = AddChannelVC()
-        addChannel.modalPresentationStyle = .custom
-        present(addChannel, animated: true, completion: nil)
+        if AuthService.instance.isLoggedIn {
+            let addChannel = AddChannelVC()
+            addChannel.modalPresentationStyle = .custom
+            present(addChannel, animated: true, completion: nil)
+        }
     }
     
     @IBAction func loginBtnWasPressed(_ sender: Any) {
@@ -54,6 +57,11 @@ class ChannelVC: UIViewController {
        setupUserInfo()
     }
     
+    @objc
+    func channelsLoaded(_ notif: Notification) {
+        tableView.reloadData()
+    }
+    
     func setupUserInfo() {
         if AuthService.instance.isLoggedIn {
             loginBtn.setTitle(UserDataService.instance.name, for: .normal)
@@ -63,6 +71,7 @@ class ChannelVC: UIViewController {
             loginBtn.setTitle("Login", for: .normal)
             userImg.image = UIImage(named: "menuProfileIcon")
             userImg.backgroundColor = UIColor.clear
+            tableView.reloadData()
         }
     }
     
@@ -85,6 +94,13 @@ extension ChannelVC: UITableViewDelegate, UITableViewDataSource {
         let channel = MessageService.instance.channels[indexPath.row]
         cell.configureCell(channel: channel)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let channel = MessageService.instance.channels[indexPath.row]
+        MessageService.instance.selectedChannel = channel
+        NotificationCenter.default.post(name: NOTIF_CHANNEL_SELECTED, object: nil)
+        self.revealViewController().revealToggle(animated: true)
     }
     
     
